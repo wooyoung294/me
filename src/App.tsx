17f782bd6f0b3ed9c93007d6b80bc14b8603238d
wooyoung <andy241822@gmail.com>
2024-03-17 02:20:27 +0900

    Styled_Component 적용으로 인한 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-// import './App.css';
-import profile from './KakaoTalk_20240311_023648831.png'
+import React, {useState} from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+import 'swiper/css/effect-creative';
 import 'swiper/css/mousewheel';
-import './App.css';
 import {Swiper, SwiperSlide} from "swiper/react";
 import {
     A11y,
+    EffectFade,
     Mousewheel,
     Navigation,
     Pagination,
     Scrollbar,
 } from 'swiper/modules';
+
 import type {Swiper as SwiperClass} from "swiper/types";
+import SwiperSlideItem from "./components/SwiperSlideItem";
+import {myProfile} from "./data/myProfile";
+import {CustomNavigator, NavigatorItem} from "./components/CustomNavigator";
+import {VisitButton} from "./components/VisitButton";
 function App() {
+
     const [step, setStep]=
         useState<number>(0)
+
     return (
         <Swiper
             className={'swiperWrapper'}
             direction={'vertical'}
-            modules={[Pagination, Navigation, Mousewheel, Scrollbar, A11y]}
+            modules={[EffectFade,Pagination, Navigation, Mousewheel, Scrollbar, A11y]}
             spaceBetween={200}
             slidesPerView={1}
             scrollbar={{draggable: true}}
@@ -30,48 +36,33 @@ function App() {
                 invert: false
             }}
             loop={true}
+            effect={'fade'}
+            fadeEffect={{
+                crossFade: true,
+            }}
             // activeIndex는 loop가 true일때 제대로 반영이 안됨 따라서 realIndex 사용
             // onSlideChange={(e:SwiperClass) => setStep(e.activeIndex)}
-            onSlideChange={(e:SwiperClass) => setStep(e.realIndex)}
-            effect="flip"
+            onSlideChange={(e:SwiperClass) => {
+                setStep(e.realIndex);
+            }}
         >
-            <SwiperSlide >
-                <div className={'dAc'}>
-                    <div className={'sliderItem'}>
-                        <img className={'sliderIMG'}
-                             src={profile}
-                             alt=""/>
-                        <div>
-                            <h3>Title</h3>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                            <div>dsafdsfasdfsajkfhasdjklfsdjklfa;sdfjasdklfjla;skdfjklsajf</div>
-                        </div>
-                    </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide >
-                <div className={'dAc'}>
-                    <div className={'sliderItem'}>
-                        <img className={'sliderIMG'}
-                             src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp"
-                             alt=""/>
-                        <div>
-                            <h3>Title2</h3>
-                        </div>
-                    </div>
-                </div>
-            </SwiperSlide>
-            <div className={'aaa'}>
-                <div className={step===0?'ccc':'bbb'}/>
-                <div className={step===1?'ccc':'bbb'}/>
-                <div className={step===2?'ccc':'bbb'}/>
-            </div>
+            {
+                myProfile.map((swiperSlideVo)=>
+                    <SwiperSlide key={swiperSlideVo.title}>
+                        <SwiperSlideItem swiperSlideVo={swiperSlideVo}/>
+                        {
+                            swiperSlideVo.visit && <VisitButton href={swiperSlideVo.visit}>방문하기</VisitButton>
+                        }
+                    </SwiperSlide>
+                )
+            }
+            <CustomNavigator>
+            {
+                myProfile.map((swiperSlideVo,idx)=>
+                    <NavigatorItem idx={idx} step={step}/>
+                )
+            }
+            </CustomNavigator>
         </Swiper>
     );
 }
