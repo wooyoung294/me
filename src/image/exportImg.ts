0ee198b3284dda66e89c8f5135bcbe37c989bdee
wooyoung <andy241822@gmail.com>
2024-03-17 02:22:04 +0900

    Img 추가 및 exportImg.ts 생성

---
+++ src/image/exportImg.ts
@@ -0,0 +1,14 @@
+import BootStrapImg from './bs.svg';
+import JavaScriptImg from './js.svg';
+import TypeScriptImg from './ts.svg';
+import ReactImg from './re.svg';
+import NextJsImg from './ne.svg';
+import ReactQueryImg from './rq.svg';
+import Avatar from './title1.png';
+import YoriJoriLogo from './YoriJoriLogo.png';
+import SpringImg from './sp.svg';
+import WebSocketImg from './ws.svg';
+import AuctionLogo from './AuctionLogo.png';
+import TestCafeLogo from './tc.png';
+
+export {BootStrapImg,JavaScriptImg,TypeScriptImg,ReactImg,ReactQueryImg,NextJsImg,Avatar,YoriJoriLogo,SpringImg,WebSocketImg,AuctionLogo,TestCafeLogo}
